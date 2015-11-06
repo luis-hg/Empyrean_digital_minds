0980be7d59a74d72f434bc04ef33b2b372e29e1e
luis-hg <luisar@luisar-thi-m.cmass.criticalmass.com>
2015-11-06 11:53:00 -0600

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,25 @@
 $(function(){
-	console.log('HI JQUERY');
-})
+	'use strict';
+
+	//Nav bar
+	var menu = $('nav.mobile').css('display','none');
+	var img = $('#logo-responsive');   
+
+	$(img).on('click', function(){
+		var $that = $(this);
+		$($that).toggleClass('rotate');
+
+
+
+		//conditionals
+		if($($that).hasClass('rotate')){
+			$($that).removeClass('reverse');
+			$(menu).slideDown('fast');
+		
+		} else{
+			$($that).addClass('reverse');
+			$(menu).slideUp('fast');
+		}
+
+	});
+});
